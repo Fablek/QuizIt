e986fd1b639875a7bfbe3d0c88240e7cdab39a03
Sebastian Pytka <76405637+Fablek@users.noreply.github.com>
2025-03-30 20:20:24 +0200

    :sparkles: edit/remove flashcards

--- QuizIt/Views/FlashcardsView.xaml.cs
+++ QuizIt/Views/FlashcardsView.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 using Microsoft.EntityFrameworkCore;
 using QuizIt.Data;
 using QuizIt.Models;
+using Microsoft.VisualBasic;
 
 namespace QuizIt.Views
 {
@@ -37,26 +38,78 @@ namespace QuizIt.Views
                     db.SaveChanges();
                 }
 
-                var main = Application.Current.MainWindow as MainWindow;
-                var viewModel = main.DataContext as ViewModels.MainViewModel;
-                viewModel.ReloadAllData();
-
-                var updatedDeck = viewModel.Decks.FirstOrDefault(d => d.Id == _deck.Id);
-                if (updatedDeck != null)
-                {
-                    main.MainContentControl.Content = new FlashcardsView(updatedDeck);
-                }
-
+                ReloadView();
                 NewFlashcardTitleBox.Text = "";
             }
         }
 
-        private void Flashcard_DoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void OpenFlashcard_Click(object sender, RoutedEventArgs e)
         {
-            if (((ListBox)sender).SelectedItem is Flashcard selectedFlashcard)
+            if ((sender as Button)?.DataContext is Flashcard selectedFlashcard)
             {
                 var main = App.Current.MainWindow as MainWindow;
                 main.MainContentControl.Content = new FlashcardDetailsView(selectedFlashcard);
+            }
+        }
+
+        private void EditFlashcard_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as Button)?.DataContext is Flashcard flashcard)
+            {
+                string newTitle = Interaction.InputBox(
+                    "Podaj nowy tytuł fiszki:", "Edytuj fiszkę", flashcard.Title);
+
+                if (!string.IsNullOrWhiteSpace(newTitle))
+                {
+                    using (var db = new AppDbContext())
+                    {
+                        var flashInDb = db.Flashcards.FirstOrDefault(f => f.Id == flashcard.Id);
+                        if (flashInDb != null)
+                        {
+                            flashInDb.Title = newTitle;
+                            db.SaveChanges();
+                        }
+                    }
+
+                    ReloadView();
+                }
+            }
+        }
+
+        private void DeleteFlashcard_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as Button)?.DataContext is Flashcard flashcard)
+            {
+                var result = MessageBox.Show($"Czy na pewno chcesz usunąć fiszkę '{flashcard.Title}'?",
+                                             "Usuń fiszkę", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    using (var db = new AppDbContext())
+                    {
+                        var toRemove = db.Flashcards.FirstOrDefault(f => f.Id == flashcard.Id);
+                        if (toRemove != null)
+                        {
+                            db.Flashcards.Remove(toRemove);
+                            db.SaveChanges();
+                        }
+                    }
+
+                    ReloadView();
+                }
+            }
+        }
+
+        private void ReloadView()
+        {
+            var main = Application.Current.MainWindow as MainWindow;
+            var viewModel = main.DataContext as ViewModels.MainViewModel;
+            viewModel.ReloadAllData();
+
+            var updatedDeck = viewModel.Decks.FirstOrDefault(d => d.Id == _deck.Id);
+            if (updatedDeck != null)
+            {
+                main.MainContentControl.Content = new FlashcardsView(updatedDeck);
             }
         }
     }
